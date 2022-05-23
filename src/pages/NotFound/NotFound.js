b765c5c2446af2c7278304fb3f1a40d93a1d2f4f
MdTasib <ohidulalam442@gmail.com>
2022-05-23 23:01:05 +0600

    not found page desing improve

--- src/pages/NotFound/NotFound.js
+++ src/pages/NotFound/NotFound.js
@@ -1,10 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const NotFound = () => {
 	return (
-		<div className='text-center p-5'>
-			<h2>Page Not Found</h2>
-			<h1>404</h1>
+		<div class='d-flex align-items-center justify-content-center vh-100'>
+			<div class='text-center'>
+				<h1 class='display-1 fw-bold'>404</h1>
+				<p class='fs-3 fw-bold'>
+					{" "}
+					<span class='text-danger'>Opps!</span> Page not found.
+				</p>
+				<p class='lead'>The page you’re looking for doesn’t exist.</p>
+				<Link to='/' class='btn btn-dark'>
+					Go Home
+				</Link>
+			</div>
 		</div>
 	);
 };
