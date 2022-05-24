9ef987e73181b14544a3acae15f372871f856187
MdTasib <ohidulalam442@gmail.com>
2022-05-24 22:29:06 +0600

    manage order complete

--- src/pages/Dashboard/OrderRow.js
+++ src/pages/Dashboard/OrderRow.js
@@ -29,8 +29,36 @@ const OrderRow = ({ order, index, refetch }) => {
 		});
 	};
 
+	// update purchase product status
 	const handleShipped = id => {
-		console.log(order);
+		Swal.fire({
+			title: "Are you sure?",
+			text: "You Shipped This Product",
+			icon: "warning",
+			showCancelButton: true,
+			confirmButtonColor: "#d33",
+			cancelButtonColor: "#3085d6",
+			confirmButtonText: "Yes, shipped it!",
+		}).then(result => {
+			if (result.isConfirmed) {
+				const shippedProduct = {
+					...order,
+					status: "shipped",
+				};
+
+				fetch(`http://localhost:5000/purchases/${id}`, {
+					method: "PUT",
+					headers: {
+						"content-type": "application/json",
+						authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+					},
+					body: JSON.stringify(shippedProduct),
+				})
+					.then(res => res.json())
+					.then(data => refetch());
+				Swal.fire("Shipped!", "Product has been shipped.", "success");
+			}
+		});
 	};
 
 	return (
@@ -39,18 +67,20 @@ const OrderRow = ({ order, index, refetch }) => {
 			<td>{order.userEmail}</td>
 			<td>{order.name}</td>
 			<td>
-				{order.paid ? (
-					<>
-						<button className='btn btn-sm btn-outline-info fw-bold me-2'>
-							PENDING...
-						</button>
-						<button
-							onClick={() => handleShipped(order._id)}
-							className='btn btn-sm btn-outline-success fw-bold'>
-							SHIPPED
-						</button>
-					</>
-				) : (
+				{order.status === "pending" && (
+					<button className='btn btn-sm btn-outline-info fw-bold me-2'>
+						PENDING...
+					</button>
+				)}
+				{(order.status === "pending" || order.status === "shipped") && (
+					<button
+						onClick={() => handleShipped(order._id)}
+						disabled={order.status === "shipped"}
+						className='btn btn-sm btn-outline-success fw-bold'>
+						SHIPPED
+					</button>
+				)}
+				{!order.paid && (
 					<button className='btn btn-sm btn-outline-dark fw-bold'>
 						UNPAID
 					</button>
